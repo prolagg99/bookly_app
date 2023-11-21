b5b9a27920a074ca8cd6ed88106675019fc16ddd
lagg99 <pro.lagg99@gmail.com>
2023-11-21 09:07:41 +0100

    splash view ui done

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xff100B20),
+      body: SplashViewBody(),
+    );
+  }
+}
