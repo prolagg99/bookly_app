d702bdb59bace207d7a77d45a9f60743c3a477f3
lagg99 <pro.lagg99@gmail.com>
2023-12-10 08:37:18 +0100

    cached network image

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -22,13 +23,17 @@ class CustomBookImage extends StatelessWidget {
       height: higth!.h,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(radius!),
-        border: Border.all(
-          width: 1,
-          color: borderColor,
-        ),
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.cover,
+        border: Border.all(width: 1, color: borderColor),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius!),
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          fit: BoxFit.fill,
+          // placeholder: (context, url) => const Center(
+          //   child: CircularProgressIndicator(),
+          // ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
